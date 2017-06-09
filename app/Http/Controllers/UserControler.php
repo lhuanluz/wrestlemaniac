12709042712397b648023fa508efe0a695206cc8
Luan Luz <luanluzpereira@hotmail.com>
2017-06-09 16:27:42 -0300

    Criação da base

---
+++ app/Http/Controllers/UserControler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserControler extends Controller
+{
+    public function criar(){
+
+        return view('welcome');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,45 @@
+<html lang="pt">
+    <head>
+        <title>Teste</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="\css\bootstrap.css">
+        <script href="\js\bootstrap.js"></script>
+    </head>
+    <body>
+    <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">
+    <img src="\images\gunplay.png" width="400" height="100" alt="">
+  </a>
+  
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="{{url('/user/create')}}">Registrar</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+    <div class="jumbotrom">
+        <div class="container">
+            <h1>Smackdown Live 06 / 06 / 2017</h1>
+            <p>teste</p>
+        </div>
+    </div>
+    
+    </body>
+</html>
