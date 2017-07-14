98ca54f1dd9d7211764ed64af866813789ef1035
rodolfoalvesmdo <rodolfoalvesmdo@gmail.com>
2017-07-14 00:42:47 -0300

    Nova página de liga

--- resources/views/leagueHome.blade.php
+++ resources/views/leagueHome.blade.php
@@ -3,205 +3,218 @@
 @section('content')
 
     @if($userHasLeague == true)
-    
-    <!--<div class="page-header">
-            <h1 class="title">Your League<i class="fa fa-chevron-down fa-lg" aria-hidden="true"></i></h1>
-    </div>-->
-    <div class="container league-page">
-        <div class="row league-name">
-            <h2>League Name</h2>
-        </div>
 
-        <div class="row league-avatar">
-            <div class="col-md-6">
-                <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000">
-                </div> <!-- .AVATAR -->
-                <h3>{{ Auth::user()->name }}</h3>
+    <div class="container league-page">
+        <div class="row league-profile">
+
+            <div class="col-md-6 league-name">
+                <h2>League Name</h2>
+                <a href="#"><p><i class="fa fa-pencil-square-o" aria-hidden="true">&nbsp</i>Edit League</p></a>
+                <a href="#"><p>Leave League</p></a>
+            </div>
+            
+            <div class="col-md-3 league-info">
+                <h2>LEAGUE SCORE</h2>
+                <h3>50.0</h3>
             </div>
 
-            <div class="col-md-6">
+            <div class="col-md-3 league-info">
+                <div class="divisor"></div>
+                <h2>LEAGUE RANK</h2>
+                <h3>999º</h3>
+            </div>
+        </div> <!-- LEAGUE-PROFILE -->
+
+        <div class="row league-team">
+            <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
+            <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
+            <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
+            <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
+            <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
+        </div> <!-- LEAGUE-TEAM -->
+
+
+        <div class="row league-scores raw">
+            
+            <div class="col-md-9">
                 <ul>
-                    <li><a class="league-btn" href="#">Edit League</a></li>            
-                    <li><a class="league-btn" href="#">Leave League</a></li>                
+                    <li>RAW</li>
+                    <li>
+                        <p>1º</p>
+                        <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
+                        <p>Melquesedeque Gomes</p>
+                        <p>14.5</p>
+                    </li>
+
+                    <li>
+                        <p>2º</p>
+                        <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
+                        <p>Melquesedeque Gomes</p>
+                        <p>14.5</p>
+                    </li>
+
+                    <div class="divisor"></div>
+
+                    <li>
+                        <p>3º</p>
+                        <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
+                        <p>Melquesedeque Gomes</p>
+                        <p>14.5</p>
+                    </li>
+
+                    <div class="divisor"></div>
+
+                    <li>
+                        <p>4º</p>
+                        <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
+                        <p>Melquesedeque Gomes</p>
+                        <p>14.5</p>
+                    </li>
+
+                    <div class="divisor"></div>
+
+                    <li>
+                        <p>5º</p>
+                        <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
+                        <p>Melquesedeque Gomes</p>
+                        <p>14.5</p>
+                    </li>
+                    
                 </ul>
             </div>
-        </div>
 
-        <div class="row league-members">
-            <div class="col-md-6">
-                <div>
-                    <div class="league-member-avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
-                    <p>Luan Luz</p>
-                </div>
-                <div>
-                    <div class="league-member-avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
-                    <p>Arthur Ariza</p>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div>
-                    <div class="league-member-avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
-                    <p>Melquesedeque Gomes</p>
-                </div>
-                <div>
-                    <div class="league-member-avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
-                    <p>Silvânio Holanda</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="divisoria"></div>
-
-        <div class="row league-stats">
-            <div class="col-md-4">
-                <div class="league-brand-score">
-                    <div class="score"><h2>50.0</h2></div>
-                    <div class="league-rank"><p>#9</p></div>
-                </div>
-                <div class="league-members-score">
-                    <h3>RAW</h3>
-                    <ul>
-                        <li>
-                            <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
-                            <p class="member-position">1º</p>
-                            <p class="member-score">14.5</p>                            
-                        </li>
-
-                        <li>
-                            <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
-                            <p class="member-position">2º</p>
-                            <p class="member-score">14.5</p>                            
-                        </li>
-
-                        <li>
-                            <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
-                            <p class="member-position">3º</p>
-                            <p class="member-score">14.5</p>                            
-                        </li>
-
-                        <li>
-                            <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
-                            <p class="member-position">4º</p>
-                            <p class="member-score">14.5</p>                            
-                        </li>
-
-                        <li>
-                            <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
-                            <p class="member-position">5º</p>
-                            <p class="member-score">14.5</p>                            
-                        </li>
-                    </ul> <!-- RAW LEAGUE SCORE -->
-                </div> <!-- COL-MD-4 -->                
+            <div class="col-md-3 league-partial-scores">
+                <ul>
+                    <li>TOTAL SCORE</li>
+                    <div class="divisor"></div>
+                    <li>50.0</li>
+                </ul>
             </div>
 
-            <div class="col-md-4">
-                <div class="league-brand-score">
-                    <div class="score"><h2>50.0</h2></div>
-                    <div class="league-rank"><p>#9</p></div>
-                </div>
-                <div class="league-members-score">
-                    <h3>SMACKDOWN</h3>
-                    <ul>
-                        <li>
-                            <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
-                            <p class="member-position">1º</p>
-                            <p class="member-score">14.5</p>                            
-                        </li>
+        </div> <!-- LEAGUE-SCORES RAW -->
 
-                        <li>
-                            <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
-                            <p class="member-position">2º</p>
-                            <p class="member-score">14.5</p>                            
-                        </li>
 
-                        <li>
-                            <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
-                            <p class="member-position">3º</p>
-                            <p class="member-score">14.5</p>                            
-                        </li>
+        <div class="row league-scores smackdown">
+            
+            <div class="col-md-9">
+                <ul>
+                    <li>SMACKDOWN</li>
+                    <li>
+                        <p>1º</p>
+                        <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
+                        <p>Melquesedeque Gomes</p>
+                        <p>14.5</p>
+                    </li>
 
-                        <li>
-                            <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
-                            <p class="member-position">4º</p>
-                            <p class="member-score">14.5</p>                            
-                        </li>
+                    <li>
+                        <p>2º</p>
+                        <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
+                        <p>Melquesedeque Gomes</p>
+                        <p>14.5</p>
+                    </li>
 
-                        <li>
-                            <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
-                            <p class="member-position">5º</p>
-                            <p class="member-score">14.5</p>                            
-                        </li>
-                    </ul>
-                </div>  <!-- SMACKDOWN LEAGUE SCORE -->              
-            </div> <!-- COL-MD-4 -->
+                    <div class="divisor"></div>
 
-            <div class="col-md-4">
-                <div class="league-brand-score">
-                    <div class="score"><h2>50.0</h2></div>
-                    <div class="league-rank"><p>-</p></div>
-                </div>
-                <div class="league-members-score">
-                    <h3>PAY PER VIEW</h3>
-                    <ul>
-                        <li>
-                            <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
-                            <p class="member-position">1º</p>
-                            <p class="member-score">14.5</p>                            
-                        </li>
+                    <li>
+                        <p>3º</p>
+                        <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
+                        <p>Melquesedeque Gomes</p>
+                        <p>14.5</p>
+                    </li>
 
-                        <li>
-                            <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
-                            <p class="member-position">2º</p>
-                            <p class="member-score">14.5</p>                            
-                        </li>
+                    <div class="divisor"></div>
 
-                        <li>
-                            <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
-                            <p class="member-position">3º</p>
-                            <p class="member-score">14.5</p>                            
-                        </li>
+                    <li>
+                        <p>4º</p>
+                        <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
+                        <p>Melquesedeque Gomes</p>
+                        <p>14.5</p>
+                    </li>
 
-                        <li>
-                            <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
-                            <p class="member-position">4º</p>
-                            <p class="member-score">14.5</p>                            
-                        </li>
+                    <div class="divisor"></div>
 
-                        <li>
-                            <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
-                            <p class="member-position">5º</p>
-                            <p class="member-score">14.5</p>                            
-                        </li>
-                    </ul>
-                </div>  <!-- PPV LEAGUE SCORE -->              
-            </div> <!-- COL-MD-4 -->
+                    <li>
+                        <p>5º</p>
+                        <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
+                        <p>Melquesedeque Gomes</p>
+                        <p>14.5</p>
+                    </li>
+                    
+                </ul>
+            </div>
 
-        </div>
+            <div class="col-md-3 league-partial-scores">
+                <ul>
+                    <li>TOTAL SCORE</li>
+                    <div class="divisor"></div>
+                    <li>50.0</li>
+                </ul>
+            </div>
 
-        <!--<div class="membrosLiga">
-            <table class="membrosLiga-info">
-                <h5 class="btn-danger btn-lg btn-block brandTitle">RAW</h5>
-                <center>
-                    <tr>
-                        
-                        <th class="membroLigaNome"><u><h4>Name</h4></u></th>
-                        <th class="membroLigaCampo"><h4>Weekly Points</h4></th>
-                        <th class="membroLigaCampo"><h4>Total Points</h4></th>
-                           
-                    </tr>
-             
-             @foreach($membrosRaw as $membroRaw)
-                    <tr>
-                        <th class="membroLigaNome"><u><h4>{{$membroRaw->name}}</h4></u></th>
-                        <th class="membroLigaCampo"><h4>{{$membroRaw->team_points}}</h4></th>
-                        <th class="membroLigaCampo"><h4>{{$membroRaw->team_total_points}}</h4></th>
-                    </tr>
-            @endforeach
+        </div> <!-- LEAGUE-SCORES SMACKDOWN -->
 
-            </center>
-            </table>
-        </div>  DIV LUTADOR -->
-    </div>
+
+        <div class="row league-scores payperview">
+            
+            <div class="col-md-9">
+                <ul>
+                    <li>PAY PER VIEW</li>
+                    <li>
+                        <p>1º</p>
+                        <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
+                        <p>Melquesedeque Gomes</p>
+                        <p>14.5</p>
+                    </li>
+
+                    <li>
+                        <p>2º</p>
+                        <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
+                        <p>Melquesedeque Gomes</p>
+                        <p>14.5</p>
+                    </li>
+
+                    <div class="divisor"></div>
+
+                    <li>
+                        <p>3º</p>
+                        <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
+                        <p>Melquesedeque Gomes</p>
+                        <p>14.5</p>
+                    </li>
+
+                    <div class="divisor"></div>
+
+                    <li>
+                        <p>4º</p>
+                        <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
+                        <p>Melquesedeque Gomes</p>
+                        <p>14.5</p>
+                    </li>
+
+                    <div class="divisor"></div>
+
+                    <li>
+                        <p>5º</p>
+                        <div class="avatar" style="background: url({{Auth::user()->photo}}) center center no-repeat; background-size: cover; background-color: #000"></div>
+                        <p>Melquesedeque Gomes</p>
+                        <p>14.5</p>
+                    </li>
+                    
+                </ul>
+            </div>
+
+            <div class="col-md-3 league-partial-scores">
+                <ul>
+                    <li>TOTAL SCORE</li>
+                    <div class="divisor"></div>
+                    <li>50.0</li>
+                </ul>
+            </div>
+
+        </div> <!-- LEAGUE-SCORES PAY PER VIEW -->
+
+
+    </div> <!-- LEAGUE-PAGE -->
     @else
 
     <section class="container league-form">
