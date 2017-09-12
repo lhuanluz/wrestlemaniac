b2637e18f64f78c433542325d78761e276f5ab9b
aaariza <tucaariza@gmail.com>
2017-09-12 16:34:58 -0300

    Alterar Password

--- resources/views/teste.blade.php
+++ resources/views/teste.blade.php
@@ -8,7 +8,7 @@
                 </div>
         <div class="wrapper">
             <!-- FORMULARIO -->
-            <form method="post" action="{{route('nameR')}}"  name="Edit_User_Name" class="form-create">       
+            <form method="post" action="{{route('passR')}}"  name="Edit_User_Pass" class="form-create">       
                 {{ csrf_field()  }}
 
                 @if (count($errors) > 0)
@@ -21,16 +21,26 @@
                     </div>
                 @endif
 
-                <!-- Campos -->  
+                <!-- Campos --> 
                 <div class="form-group">
-                    <label>New Name</label>
-                   <input type="text" name="newName" class="form-control">                
+                    <label>Old Pass</label>
+                   <input type="password" name="oldPass" min = 6 class="form-control">                
+                </div>   
+                <div class="form-group">
+                    <label>New Pass</label>
+                   <input type="password" name="newPass" min = 6 class="form-control">                
                 </div>            
                 <!-- BOTÃO -->
                 <br/>
-                <button class="btn btn-primary btn-block btn-lg"  name="Submit" value="criar" type="Submit">Edit Name</button>     
+                <button class="btn btn-primary btn-block btn-lg"  name="Submit" value="criar" type="Submit">Edit Pass</button>     
 
             </form>         
             <!-- FORMULARIO [FIM] -->
         </div>
     </div>
+
+    @if(Session::has('message'))
+<div class="alert-box success">
+    {{ Session::get('message') }}
+</div>
+@endif
