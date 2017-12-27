1d4a5e78c051f339bb37b726a9892879927bb12f
luanluz <luanluzpereira@hotmail.com>
2017-12-27 20:03:25 -0200

    Novo Mercado #GoRodolfinho

--- resources/views/mercadoRawHome.blade.php
+++ resources/views/mercadoRawHome.blade.php
@@ -1,6 +1,127 @@
 @extends('layouts/app')
 
 @section('content')
+
+    <section class="raw-banner"></section>
+    <section class="market">
+        <div class="container-fluid market-team">
+            <h2>Your Team</h2> 
+            <div class="row">
+                <div class="col-md-3 your-team">
+                    <div class="d1">
+                        <img src="http://localhost:8000/img/roman_reigns.png" alt="Roman Reigns">
+                        <p>Roman Reigns</p>
+                    </div> 
+
+                    <ul class="d2">
+                        <li>
+                            <p>Score</p>
+                            <p>8.5</p>
+                        </li> 
+                        <li class="divisor"></li> 
+                        <li>
+                            <p>Price</p> 
+                            <p>$ 1275</p>
+                        </li>
+                    </ul> 
+                    <a href="#">Sell</a>
+                </div> 
+                
+                <div class="col-md-3 your-team">
+                    <div class="d1">
+                        <img src="http://localhost:8000/img/roman_reigns.png" alt="Roman Reigns"> 
+                        <p>Roman Reigns</p>
+                    </div> 
+                    <ul class="d2">
+                        <li>
+                            <p>Score</p> 
+                            <p>8.5</p>
+                        </li> 
+                        <li class="divisor"></li> 
+                        <li>
+                            <p>Price</p> 
+                            <p>$ 1275</p>
+                        </li>
+                    </ul> 
+                    <a href="#">Sell</a>
+                </div> 
+                
+                <div class="col-md-3 your-team">
+                    <div class="d1">
+                        <img src="http://localhost:8000/img/roman_reigns.png" alt="Roman Reigns"> 
+                        <p>Roman Reigns</p>
+                    </div> 
+                    <ul class="d2">
+                        <li>
+                            <p>Score</p> 
+                            <p>8.5</p>
+                        </li> 
+                        <li class="divisor"></li> 
+                        <li>
+                            <p>Price</p> 
+                            <p>$ 1275</p>
+                        </li>
+                    </ul> 
+                    <a href="#">Sell</a>
+                </div> 
+                
+                <div class="col-md-3 your-team">
+                    <div class="d1">
+                        <img src="http://localhost:8000/img/roman_reigns.png" alt="Roman Reigns"> 
+                        <p>Roman Reigns</p>
+                    </div> 
+                    <ul class="d2">
+                        <li>
+                            <p>Score</p> 
+                            <p>8.5</p>
+                        </li> 
+                        <li class="divisor"></li> 
+                        <li>
+                            <p>Price</p> 
+                            <p>$ 1275</p>
+                        </li>
+                    </ul> 
+                    <a href="#">Sell</a>
+                </div>
+            </div> 
+            
+            <div class="row">
+                <div class="market-info col-md-9">
+                    <ul>
+                        <li>
+                            <p>Total Cash</p> 
+                            <p>$ 7112</p>
+                        </li> 
+                        <li class="divisor"></li> 
+                        <li>
+                            <p>Cash Available</p> 
+                            <p>$ 68</p>
+                        </li> 
+                        <li class="divisor"></li> 
+                        <li>
+                            <p>Last Show Balance</p> 
+                            <p>$ 405</p>
+                        </li> 
+                        <li class="divisor"></li> 
+                        <li>
+                            <p>Market Status</p>
+                            <p class="market-open">OPEN</p>
+                        </li>                    
+                    </ul>                
+                </div> 
+                
+                <div class="col-md-3">
+                    <a href="#" class="sell-all">Sell all</a> 
+                    <a href="#" class="confirm-team">Confirm team</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
     <script type="text/javascript">
@@ -46,7 +167,7 @@
                     <!-- Mostra o nome do Superstar -->
                     <u><h4>{{$superstar->name}}</h4></u>
                     <input type="hidden" name="name" value="{{$superstar->name}}"/>
-                    <!-- Verifica se o Superstar apareceu no último show -->
+                    <!-- Verifica se o Superstar apareceu no Ãºltimo show -->
                     @if($superstar->last_show == 1)
                         <div class="alert-success">
                         <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
@@ -58,11 +179,11 @@
                         Didn't appeard on Last Show
                         </div>
                     @endif
-                    <!-- Mostra os Pontos Atuais / Últimos -->
+                    <!-- Mostra os Pontos Atuais / Ãšltimos -->
                     <h2 class="pontos">{{$superstar->points . ' / ' . $superstar->last_points}}</h2>
-                    <!-- Mostra o Preço -->
-                    <h2 class="preço" name="price"><span class="glyphicon glyphicon-usd"></span>{{$superstar->price}}</h2>
-                    <!-- Verifica se o Usuário está cadastrado, caso contrário não mostra os botões para comprar -->
+                    <!-- Mostra o PreÃ§o -->
+                    <h2 class="preÃ§o" name="price"><span class="glyphicon glyphicon-usd"></span>{{$superstar->price}}</h2>
+                    <!-- Verifica se o UsuÃ¡rio estÃ¡ cadastrado, caso contrÃ¡rio nÃ£o mostra os botÃµes para comprar -->
                     @if($superstar->name == 'None')
                     <button type="Submit" class="btn btn-danger btn-group-justified" disabled>
                     <i class="fa fa-exclamation-circle fa-lg icon" aria-hidden="true"></i>Can't Sell
@@ -86,29 +207,29 @@
         <div class="page-header">
             <h1 class="title">Market<i class="fa fa-chevron-down fa-lg" aria-hidden="true"></i></h1>
         </div>
-        <!-- Sempre veremos o botão Name▲ -->
-        <a href="{{route('mercadoRawHome')}}">Name▲</a>
-        <!-- Condiciona que os botões Price▲ e Points▲  só irão aparecer quando:
+        <!-- Sempre veremos o botÃ£o Nameâ–² -->
+        <a href="{{route('mercadoRawHome')}}">Nameâ–²</a>
+        <!-- Condiciona que os botÃµes Priceâ–² e Pointsâ–²  sÃ³ irÃ£o aparecer quando:
         -> Estivermos vendo a Home do Mercado
-        ->Estivermos Vendo o Price▼ ou Points▼
+        ->Estivermos Vendo o Priceâ–¼ ou Pointsâ–¼
         -->
         @if(Route::is('mercadoRawHome')|| Route::is('mercadoRawPriceDesc') ||Route::is('mercadoRawPointsDesc'))
-            <a href="{{route('mercadoRawPriceAsc')}}">Price▲</a>
-            <a href="{{route('mercadoRawPointsAsc')}}">Points▲</a>
+            <a href="{{route('mercadoRawPriceAsc')}}">Priceâ–²</a>
+            <a href="{{route('mercadoRawPointsAsc')}}">Pointsâ–²</a>
         @endif
-        <!-- Condiciona que os botões Price▼ e Points▲  só irão aparecer quando:
-        -> Estivermos vendo o Price▲
+        <!-- Condiciona que os botÃµes Priceâ–¼ e Pointsâ–²  sÃ³ irÃ£o aparecer quando:
+        -> Estivermos vendo o Priceâ–²
         -->
         @if(Route::is('mercadoRawPriceAsc'))
-            <a href="{{route('mercadoRawPriceDesc')}}">Price▼</a>
-            <a href="{{route('mercadoRawPointsAsc')}}">Points▲</a>
+            <a href="{{route('mercadoRawPriceDesc')}}">Priceâ–¼</a>
+            <a href="{{route('mercadoRawPointsAsc')}}">Pointsâ–²</a>
         @endif  
-        <!-- Condiciona que os botões Price▼ e Points▲  só irão aparecer quando:
-        -> Estivermos vendo o Points▲
+        <!-- Condiciona que os botÃµes Priceâ–¼ e Pointsâ–²  sÃ³ irÃ£o aparecer quando:
+        -> Estivermos vendo o Pointsâ–²
         -->
         @if(Route::is('mercadoRawPointsAsc'))
-            <a href="{{route('mercadoRawPriceAsc')}}">Price▲</a>
-            <a href="{{route('mercadoRawPointsDesc')}}">Points▼</a>
+            <a href="{{route('mercadoRawPriceAsc')}}">Priceâ–²</a>
+            <a href="{{route('mercadoRawPointsDesc')}}">Pointsâ–¼</a>
         @endif
         <div id="wrapSearch" class="mercadoSearch">
             <form action="" autocomplete="on">
@@ -132,7 +253,7 @@
                         <!-- Mostra o nome do Superstar -->
                         <u><h4>{{$superstar->name}}</h4></u>
                         <input type="hidden" name="name" value="{{$superstar->name}}"/>
-                        <!-- Verifica se o Superstar apareceu no último show -->
+                        <!-- Verifica se o Superstar apareceu no Ãºltimo show -->
                         @if($superstar->last_show == 1)
                             <div class="alert-success">
                             <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
@@ -144,13 +265,13 @@
                             Didn't appeard on Last Show
                             </div>
                         @endif
-                        <!-- Mostra os Pontos Atuais / Últimos -->
+                        <!-- Mostra os Pontos Atuais / Ãšltimos -->
                         <h2 class="pontos">{{$superstar->points . ' / ' . $superstar->last_points}}</h2>
-                        <!-- Mostra o Preço -->
-                        <h2 class="preço" name="price"><span class="glyphicon glyphicon-usd"></span>{{$superstar->price}}</h2>
-                        <!-- Verifica se o Usuário está cadastrado, caso contrário não mostra os botões para comprar -->
+                        <!-- Mostra o PreÃ§o -->
+                        <h2 class="preÃ§o" name="price"><span class="glyphicon glyphicon-usd"></span>{{$superstar->price}}</h2>
+                        <!-- Verifica se o UsuÃ¡rio estÃ¡ cadastrado, caso contrÃ¡rio nÃ£o mostra os botÃµes para comprar -->
                         @if(Auth::user())
-                            <!-- Caso seja um Superstar do RAW mostra botão vermelho -->
+                            <!-- Caso seja um Superstar do RAW mostra botÃ£o vermelho -->
                                 @if($rawTeam->superstar01 != 103 && $rawTeam->superstar02 != 102 && $rawTeam->superstar03 != 101 && $rawTeam->superstar04 != 100 )
                                     <button type="Submit" class="btn btn-danger btn-group-justified" disabled>
                                     <i class="fa fa-exclamation-circle fa-lg icon" aria-hidden="true"></i>Not enough space
