38044ece632c036b258b13990a2eda7bd2782fdc
luanluz <luanluzpereira@hotmail.com>
2018-01-24 02:28:21 -0200

    Rank League Configurado

--- resources/views/statistics.blade.php
+++ resources/views/statistics.blade.php
@@ -28,25 +28,53 @@
             <div class="top3">
                 <div class="row">
                     <ul>
-                        <li>
-                            <img src="{{ url('img/roman_reigns3.png') }}" alt="User Icon">
-                            <p>2º</p>
-                            <p>Melquesedeque Gomes</p>
-                            <p>999.99</p>
-                        </li>
-                        <li>
-                            <img src="{{ url('img/roman_reigns3.png') }}" alt="User Icon">
-                            <p>1º</p>
-                            <img src="/storage/belts/raw.png" class="belt-icon">
-                            <p>Melquesedeque Gomes</p>
-                            <p>999.99</p>
-                        </li>
-                        <li>
-                            <img src="{{ url('img/roman_reigns3.png') }}" alt="User Icon">
-                            <p>3º</p>
-                            <p>Melquesedeque Gomes</p>
-                            <p>999.99</p>
-                        </li>
+                        <?php
+                        $posRaw = 1; 
+                        ?>
+                        @foreach($topRawTotalPoints as $topRawTotalPoint) 
+                            @if($posRaw == 2)
+                                <li>
+                                    <img src="{{ url($topRawTotalPoint->photo) }}" alt="User Icon">
+                                    <p>2º</p>
+                                    <p>{{$topRawTotalPoint->name}}</p>
+                                    <p>{{$topRawTotalPoint->team_total_points}}</p>
+                                </li>
+                            @endif
+                            <?php $posRaw++; ?>
+                        @endforeach
+
+                        <?php
+                        $posRaw = 1; 
+                        ?>
+
+                        @foreach($topRawTotalPoints as $topRawTotalPoint) 
+                            @if($posRaw == 1)
+                                <li>
+                                    <img src="{{ url($topRawTotalPoint->photo) }}" alt="User Icon">
+                                    <p>1º</p>
+                                    <img src="/storage/belts/raw.png" class="belt-icon">
+                                    <p>{{$topRawTotalPoint->name}}</p>
+                                    <p>{{$topRawTotalPoint->team_total_points}}</p>
+                                </li>
+                            @endif
+                            <?php $posRaw++; ?>
+                        @endforeach
+
+                        <?php
+                        $posRaw = 1; 
+                        ?>
+
+                        @foreach($topRawTotalPoints as $topRawTotalPoint) 
+                            @if($posRaw == 3)
+                                <li>
+                                    <img src="{{ url($topRawTotalPoint->photo) }}" alt="User Icon">
+                                    <p>3º</p>
+                                    <p>{{$topRawTotalPoint->name}}</p>
+                                    <p>{{$topRawTotalPoint->team_total_points}}</p>
+                                </li>
+                            @endif
+                            <?php $posRaw++; ?>
+                        @endforeach
                     </ul>
                 </div>
             </div>
@@ -60,20 +88,21 @@
             </div>
 
             <div class="top10">
-
-                <ul>
-                    <li>4º</li>
-                    <li><img src="{{ url('img/roman_reigns3.png') }}" alt=""></li>
-                    <li>Melquesedeque Gomes</li>
-                    <li>999.99</li>
-                </ul>
-
-                <ul>
-                    <li>5º</li>
-                    <li><img src="{{ url('img/roman_reigns3.png') }}" alt=""></li>
-                    <li>Melquesedeque Gomes</li>
-                    <li>999.99</li>
-                </ul>
+                <?php
+                $posRaw = 1; 
+                ?>
+                @foreach($topRawTotalPoints as $topRawTotalPoint) 
+                    @if($posRaw < 4)
+                    @else
+                        <ul>
+                            <li>{{$posRaw}}º</li>
+                            <li><img src="{{ url($topRawTotalPoint->photo) }}" alt=""></li>
+                            <li>{{$topRawTotalPoint->name}}</li>
+                            <li>{{$topRawTotalPoint->team_total_points}}</li>
+                        </ul>
+                    @endif
+                    <?php $posRaw++; ?>
+                @endforeach
             </div>            
         </div>
 
@@ -81,27 +110,55 @@
 
             <div class="top3">
                 <div class="row smd">
-                    <ul>
-                        <li>
-                            <img src="{{ url('img/roman_reigns3.png') }}" alt="User Icon">
-                            <p>2º</p>
-                            <p>Luan Luz</p>
-                            <p>999.99</p>
-                        </li>
-                        <li>
-                            <img src="{{ url('img/roman_reigns3.png') }}" alt="User Icon">
-                            <p>1º</p>
-                            <img src="/storage/belts/smackdown.png" class="belt-icon">
-                            <p>Luan Luz</p>
-                            <p>999.99</p>
-                        </li>
-                        <li>
-                            <img src="{{ url('img/roman_reigns3.png') }}" alt="User Icon">
-                            <p>3º</p>
-                            <p>Luan Luz</p>
-                            <p>999.99</p>
-                        </li>
-                    </ul>
+                        <ul>
+                                <?php
+                                $posSmack = 1; 
+                                ?>
+                                @foreach($topSmackdownTotalPoints as $topSmackdownTotalPoint)
+                                    @if($posSmack == 2)
+                                        <li>
+                                            <img src="{{ url($topSmackdownTotalPoint->photo) }}" alt="User Icon">
+                                            <p>2º</p>
+                                            <p>{{$topSmackdownTotalPoint->name}}</p>
+                                            <p>{{$topSmackdownTotalPoint->team_total_points}}</p>
+                                        </li>
+                                    @endif
+                                    <?php $posSmack++; ?>
+                                @endforeach
+        
+                                <?php
+                                $posSmack = 1; 
+                                ?>
+        
+                                @foreach($topSmackdownTotalPoints as $topSmackdownTotalPoint)
+                                    @if($posSmack == 1)
+                                        <li>
+                                            <img src="{{ url($topSmackdownTotalPoint->photo) }}" alt="User Icon">
+                                            <p>1º</p>
+                                            <img src="/storage/belts/smackdown.png" class="belt-icon">
+                                            <p>{{$topSmackdownTotalPoint->name}}</p>
+                                            <p>{{$topSmackdownTotalPoint->team_total_points}}</p>
+                                        </li>
+                                    @endif
+                                    <?php $posSmack++; ?>
+                                @endforeach
+        
+                                <?php
+                                $posSmack = 1; 
+                                ?>
+        
+                                @foreach($topSmackdownTotalPoints as $topSmackdownTotalPoint)
+                                    @if($posSmack == 3)
+                                        <li>
+                                            <img src="{{ url($topSmackdownTotalPoint->photo) }}" alt="User Icon">
+                                            <p>3º</p>
+                                            <p>{{$topSmackdownTotalPoint->name}}</p>
+                                            <p>{{$topSmackdownTotalPoint->team_total_points}}</p>
+                                        </li>
+                                    @endif
+                                    <?php $posSmack++; ?>
+                                @endforeach
+                            </ul>
                 </div>
             </div>
 
@@ -114,20 +171,21 @@
             </div>
 
             <div class="top10">
-
-                <ul class="smd">
-                    <li>4º</li>
-                    <li><img src="{{ url('img/roman_reigns3.png') }}" alt=""></li>
-                    <li>Melquesedeque Gomes</li>
-                    <li>999.99</li>
-                </ul>
-
-                <ul class="smd">
-                    <li>5º</li>
-                    <li><img src="{{ url('img/roman_reigns3.png') }}" alt=""></li>
-                    <li>Melquesedeque Gomes</li>
-                    <li>999.99</li>
-                </ul>
+                    <?php
+                    $posSmack = 1; 
+                    ?>
+                    @foreach($topSmackdownTotalPoints as $topSmackdownTotalPoint)
+                        @if($posSmack < 4)
+                        @else
+                        <ul class="smd">
+                                <li>{{$posSmack}}º</li>
+                                <li><img src="{{ url($topSmackdownTotalPoint->photo) }}" alt=""></li>
+                                <li>{{$topSmackdownTotalPoint->name}}</li>
+                                <li>{{$topSmackdownTotalPoint->team_total_points}}</li>
+                        </ul>
+                        @endif
+                        <?php $posSmack++; ?>
+                    @endforeach
             </div>
         </div>
 
@@ -142,134 +200,30 @@
             </div>
 
             <div class="top10-league">
-
-                <ul class="league-champ">
-                    <li>1º</li>
-                    <li>Melquesedeque Gomes</li>
-                    <li><img src="/storage/belts/league.png" alt=""></li>
-                    <li>999.99</li>
-                </ul>
-
-                <ul>
-                    <li>2º</li>
-                    <li>Melquesedeque Gomes</li>
-                    <li>999.99</li>
-                </ul>
+                <?php
+                $posL = 1; 
+                ?>
+                @foreach($topLeagues as $topLeague)
+                    @if($posL == 1)
+                        <ul class="league-champ">
+                            <li>{{$posL}}º</li>
+                            <li>{{$topLeague->league_name}}</li>
+                            <li><img src="/storage/belts/league.png" alt=""></li>
+                            <li>{{number_format($topLeague->league_points, 2, ',', ' ')}}</li>
+                        </ul>
+                    @else
+                    <ul>
+                        <li>{{$posL}}º</li>
+                        <li>{{$topLeague->league_name}}</li>
+                        <li>{{number_format($topLeague->league_points, 2, ',', ' ')}}</li>
+                    </ul>
+                    @endif
+                <?php $posL = $posL+1; ?>
+                @endforeach
             </div>
         </div>
     </div>
 
 </section>
-
-
-
-
-<!-- PÁGINA RANK ANTIGA -->
-
-<div class="container-fluid rank">
-    <div class="row league-rank">
-            <ul>
-                <li>LEAGUE RANK</li>
-                <?php
-                $posL = 1; 
-                ?>
-                @foreach($topLeagues as $topLeague)
-
-                <li>
-                    <p>{{$posL}}º</p>
-                    @if($posL == 1)
-                        <img class="imgRankBeltLeague" src="/storage/belts/league.png"/>
-                    @endif
-                    <p>{{$topLeague->league_name}}</p>
-                    <p>{{number_format($topLeague->league_points, 2, ',', ' ')}}</p>
-                </li>
-
-                <?php $posL = $posL+1; ?>
-                @endforeach
-            </ul>
-    </div>
-
-    <div class="row league-brands">
-        <div class="league-raw col-md-6">
-                <ul>
-                    <li>RAW</li>
-                    <?php
-                    $pos = 1; 
-                    ?>
-                    @foreach($topRawTotalPoints as $topRawTotalPoint) 
-                    <li>
-                        <p>{{$pos}}º</p>
-                        @if($pos == 1)
-                        <img class="imgRankBelt" src="/storage/belts/raw.png"/>
-                        @endif
-                        <div class="avatar" style="background: url({{$topRawTotalPoint->photo}}) center center no-repeat; background-size: cover;"></div>
-                        <p>{{$topRawTotalPoint->name}}</p>
-                        <p>{{$topRawTotalPoint->team_total_points}}</p>
-                    </li>
-
-                    <?php $pos++; ?>
-                    @endforeach
-                </ul>
-        </div>
-
-        <div class="league-smd col-md-6">
-                <ul>
-                    <li>SMACKDOWN</li>
-                    <?php
-                    $pos = 1; 
-                    ?>
-                    @foreach($topSmackdownTotalPoints as $topSmackdownTotalPoint)
-                    
-                    <li>
-                        <p>{{$pos}}º</p>
-                        @if($pos == 1)
-                        <img class="imgRankBelt" src="/storage/belts/smackdown.png"/>
-                        @endif
-                        <div class="avatar" style="background: url({{$topSmackdownTotalPoint->photo}}) center center no-repeat; background-size: cover;"></div>
-                        <p>{{$topSmackdownTotalPoint->name}}</p>
-                        <p>{{$topSmackdownTotalPoint->team_total_points}}</p>
-                    </li>
-
-                    <?php $pos++; ?>
-                    @endforeach
-                </ul>
-        </div>
-    </div>
-
-
-
-
-
-
-
-        <!-- <div class="row league-scores raw">
-
-        <div class="col-md-9">
-            <ul>
-                <li>RAW</li>
-                <li>
-                    <p>99º</p>
-                    <div class="avatar" style="background: url() center center no-repeat; background-size: cover; background-color: #000"></div>
-                    <p>Rodolfo Alves</p>
-                    <p>9999.99</p>
-                </li>
-
-                <li>
-                    <p>99º</p>
-                    <div class="avatar" style="background: url() center center no-repeat; background-size: cover; background-color: #000"></div>
-                    <p>Rodolfo Alves</p>
-                    <p>9999.99</p>
-                </li>
-            </ul>
-        </div> -->
-    </div>
-
-
-
-
-
-
-
-</div>
 
 @endsection
